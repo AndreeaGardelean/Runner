2bb1b09b912a2c2349f83fdb658c4a24dfb11a8f
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-09-13 14:53:18 +0100

    Created a start button interface for starting activity

--- runner-ui/src/App.js
+++ runner-ui/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import TopBar from './components/TopBar';
+import StartBar from './components/StartBar';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <StartBar />
       <NavBar />
     </div>
   );
---
+++ runner-ui/src/components/StartBar.js
@@ -0,0 +1,11 @@
+import '../style/startBar.css';
+
+export default function StartBar() {
+  return (
+    <div className="star-container" >
+      <div className="start-bar">      
+        <p>Start</p>
+      </div>
+    </div>
+  )
+}
