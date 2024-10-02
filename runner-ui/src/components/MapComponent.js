c26a20299cb1e3469536b0ca97dfb85201c16a33
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-10-02 16:23:43 +0100

    remove map pointer, and updated map coordinates

--- runner-ui/src/components/MapComponent.js
+++ runner-ui/src/components/MapComponent.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as maptilersdk from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 import '../style/map.css';
@@ -7,12 +7,12 @@ import '../style/map.css';
  * MapComponent function creates and displays a map using MapTiler SDK library.
  * The map is styled using an external CSS file and is wrapped in a container.
  *
- * @returns {JSX.Element} a map wrapped in an div element container.
+ * @returns {JSX.Element} a map wrapped in a div element container.
  */
 export default function MapComponent() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const coords = { lng: 139.753, lat: 35.6844 };
+  const [coords, setCoords] = useState({ lat: 51.5085, lng: -0.1257 });
   const zoom = 14;
 
   maptilersdk.config.apiKey = 'ujUSnZiX5ExUZRNUP4Tl';
@@ -24,21 +24,15 @@ export default function MapComponent() {
 
     map.current = new maptilersdk.Map({
       container: mapContainer.current,
-      style: maptilersdk.MapStyle.STREETS,
+      style: maptilersdk.MapStyle.BASIC,
       center: [coords.lng, coords.lat],
-      zoom: zoom
+      zoom: zoom,
     });
-
-    new maptilersdk.Marker({color: "#90c6af"})
-      .setLngLat([139.7525,35.6846])
-      .addTo(map.current);
-    
   }, [coords.lng, coords.lat, zoom]);
-
 
   return (
     <div className='map-container'>
       <div ref={mapContainer} className='map' />
     </div>
-  )
+  );
 }
