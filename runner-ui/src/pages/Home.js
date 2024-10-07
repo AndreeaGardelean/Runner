3740462d8c2f7858248509bdc8f64de658140b26
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-10-07 19:37:31 +0100

    Created a function for adding hard-coded activity data to the database using the API, this is done for testing purposes

--- runner-ui/src/pages/Home.js
+++ runner-ui/src/pages/Home.js
@@ -7,10 +7,44 @@ import StartBar from '../components/StartBar.js';
  * @returns <div> element containing elements of the home page
  */
 export default function Home() {
+  const activityData = {
+    title: 'The first activity',
+    date: 'Oct 01, 2024 at 11:41',
+    distance: '4.10',
+    time: '30:55',
+    pace: '7:20',
+    calories: '200'
+  }
+
+  async function saveActivity() {
+    try {
+      
+      console.log(localStorage.getItem('userId'))
+        const response = await fetch(`http://localhost:8080/runner/activity/create?userId=${localStorage.getItem("userId")}`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(activityData),
+          credentials: 'include'
+        });
+
+        if (response.status === 201) {
+            console.log('ACTIVITY ADDED!');
+        } else {
+            console.error('Failed to add activity:', response.status, response.statusText);
+        }
+
+    } catch (e) {
+        console.error('UNABLE TO SAVE ACTIVITY => ', e);
+    }
+}
+
+
   return (
     <div>
       <MapComponent />
-      <StartBar />
+      <StartBar onClick={saveActivity} />
     </div>
   )
 }
